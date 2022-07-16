68625a8c7831d989a4540470706d5d987da79393
Adam Whittingham <adam.whittingham@gmail.com>
2022-07-16 12:10:35 +0100

    Add hold A for Alt

--- crkbd/Ad01/keymap.c
+++ crkbd/Ad01/keymap.c
@@ -26,6 +26,7 @@ const uint16_t SPCCMD = CMD_T(KC_SPC);
 const uint16_t ALFRED = LCMD(KC_SPC);
 const uint16_t MODSFT = OSM(MOD_LSFT);
 const uint16_t SHF_S = SFT_T(KC_S);
+const uint16_t ALT_A = ALT_T(KC_A);
 const uint16_t LPAB = MT(KC_LABK, KC_LPRN);
 const uint16_t RPAB = MT(KC_RABK, KC_RPRN);
 
@@ -57,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,   SHF_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LCTL,   ALT_A,   SHF_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  ESCCMD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
